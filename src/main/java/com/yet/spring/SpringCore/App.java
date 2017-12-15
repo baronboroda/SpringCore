ebc58edff823042d17c388cdbb13a4fe7f546c18
maksym.zelenin <maksym.zelenin@Expedition.edvantis.com>
2017-12-15 19:38:19 +0200

    POM.xml was created

---
+++ src/main/java/com/yet/spring/SpringCore/App.java
@@ -0,0 +1,13 @@
+package com.yet.spring.SpringCore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
